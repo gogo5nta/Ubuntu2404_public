6cc6a9923ba0135396298f0b45d9f42a724f05ce
Tomohide Ishigami <gogo5nta@gmail.com>
2026-03-22 23:03:07 +0900

    add iinstall docker

--- 01_ubuntu_setup/02_install_nvida_container_toolkit.sh
+++ 01_ubuntu_setup/02_install_nvida_container_toolkit.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 # update 2026.03.23 22:00JST
 
+
 # ----------------------------------
-# echo "# Install NVIDIA Container Toolkit
+# echo "Install docker
 # ----------------------------------
-echo "# Install NVIDIA Container Toolkit"
+echo "Install docker"
+
+# Dockerをインストール
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD8
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+# 確認(Docker)
+sudo docker run hello-world
+
+# 確認(Docker version)
+docker -v
+
+# ----------------------------------
+# echo "Install NVIDIA Container Toolkit
+# ----------------------------------
+echo "Install NVIDIA Container Toolkit"
 
 # Reffernce
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installation
